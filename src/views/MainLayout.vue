b5649cf61a7984f6342094287e11c1b936fa346f
Yuntsy <207946431@qq.com>
2025-01-09 09:52:25 +0800

    MainLayout Page

--- src/views/MainLayout.vue
+++ src/views/MainLayout.vue
@@ -1,29 +1,12 @@
 <template>
-  <div class="main-layout">
-    <NavBar />
-    <div class="main-content">
+  <div class="min-h-screen bg-[#13132B]">
+    <TopNavBar class="px-10 topnavbar" />
+    <div class="w-full">
       <router-view></router-view>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import NavBar from '@/components/NavBar.vue'
+import TopNavBar from '@/components/TopNavBar.vue'
 </script>
-
-<style scoped>
-.main-layout {
-  /* min-height: 200vh; */
-  background: #13132B;
-}
-
-.main-content {
-  width: 100%;
-  /* 移除所有内边距，让子页面自己控制布局 */
-}
-
-/* NavBar 的样式可以单独控制 */
-:deep(.nav-bar) {
-  padding: 0 40px;
-}
-</style> 
