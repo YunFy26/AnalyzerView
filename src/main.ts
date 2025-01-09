69ff7e67232584f9f75ffa85d74577cb05de906e
Yuntsy <207946431@qq.com>
2025-01-09 09:53:18 +0800

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,35 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faFolder,
+  faMagnifyingGlass,
+  faRoute,
+  faPuzzlePiece,
+  faArrowsToCircle,
+  faDiagramProject,
+  faScissors,
+  faRobot,
+  faShieldHalved
+} from '@fortawesome/free-solid-svg-icons'
+import '../tailwind.css'
+
+// 添加图标到库中
+library.add(
+  faFolder,
+  faMagnifyingGlass,
+  faRoute,
+  faPuzzlePiece,
+  faArrowsToCircle,
+  faDiagramProject,
+  faScissors,
+  faRobot,
+  faShieldHalved
+)
 
 const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(router)
-app.mount('#app') 
+app.mount('#app')
